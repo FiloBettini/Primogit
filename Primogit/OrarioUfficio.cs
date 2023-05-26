7e805ab64426842c8b089e52292782a93c130e55
l.lazzari <l.lazzari@mindgear.it>
2023-05-26 17:33:01 +0200

    Promo Commit : Collegamento a DB

---
+++ Primogit/OrarioUfficio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Primogit
+{
+    public class OrarioUfficio
+    {
+        public int Id { get; set; }
+        public DayOfWeek GiornoSettimana { get; set; }
+        public TimeSpan Ora_Da { get; set; }
+        public TimeSpan Ora_A { get; set; }
+        public double TotaleOre { get; set; }
+    }
+}
--- Primogit/Program.cs
+++ Primogit/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace Primogit
+﻿using Microsoft.Data.SqlClient;
+using System.Reflection.PortableExecutable;
+
+namespace Primogit
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string connectionString = "data Source=DESKTOP-J5O3RI2;initial catalog=PrimoDB;uid=sa;pwd=duma2008sa;TrustServerCertificate=true";
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string command = "SELECT Id, GiornoSettimana, Ora_Da, Ora_A, TotaleOre FROM Prima_Tabella";
+
+                SqlCommand cmd = new SqlCommand(command, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                List<OrarioUfficio> orari_uffico = new List<OrarioUfficio>();
+
+                while (reader.Read())
+                {
+                    OrarioUfficio orario = new OrarioUfficio();
+
+                    orario.Id = Convert.ToInt32(reader[0]);
+                    orario.GiornoSettimana = (DayOfWeek)reader[1];
+                    orario.Ora_Da = (TimeSpan)reader[2];
+                    orario.Ora_A = (TimeSpan)reader[3];
+                    orario.TotaleOre = Convert.ToDouble(reader[4]);
+
+                    orari_uffico.Add(orario);
+                }
+
+                foreach(var item in orari_uffico) {
+                    Console.Write(item.Id + " - ");
+                    Console.Write(item.GiornoSettimana + " - ");
+                    Console.Write(item.Ora_Da + " - ");
+                    Console.Write(item.Ora_A + " - ");
+                    Console.WriteLine(item.TotaleOre);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
